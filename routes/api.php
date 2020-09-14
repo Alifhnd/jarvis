c5087dd6ff22da5627788668da9da9448703876d
ali fallah <ali.fhnd9270@gmail.com>
2020-09-14 18:08:12 +0430

    add cart item api routes

--- routes/api.php
+++ routes/api.php
@@ -28,8 +28,9 @@ Route::group(['prefix' => 'v1'], function () {
         Route::get("/{id}" , 'ProductsController@getProductById');
     });
     Route::group(["prefix" => "cart"], function (){
+        Route::post("/show" , "CartsController@show");
         Route::post("/" , "CartsController@create");
-        Route::get("/{id}" , "CartsController@create");
+        Route::post('/{id}', 'CartsController@addProduct');
     });
 
 

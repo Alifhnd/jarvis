7a25cfee79bca37b4c2cb086eb10ad6ac3416502
ali fallah <ali.fhnd9270@gmail.com>
2020-09-14 18:09:58 +0430

    add relation to cart for cart items

--- app/Model/Cart.php
+++ app/Model/Cart.php
@@ -10,6 +10,33 @@ class Cart extends Model
     use SoftDeletes;
 
     protected $fillable = [
+        "id",
         "key"
     ];
+
+    public $incrementing = false;
+
+
+    /**
+     * One-to-Many relation cart items
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function items()
+    {
+        return $this->hasMany(CartItem::class, 'cart_id');
+    }
+
+
+    /**
+     * find cart by cart key
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function findCartByKey(string $key)
+    {
+       return $this->where("key" , $key)->first();
+    }
 }
